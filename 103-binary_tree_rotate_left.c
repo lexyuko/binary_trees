74d34f14bcc6d89180380a5d0222522acdfb8e61
alexander akpabio <140617137+lexyuko@users.noreply.github.com>
2024-03-01 04:05:41 +0100

    done

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,20 +8,20 @@
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *t_right;
+	binary_tree_t *t_left;
 
-	if (tree == NULL || tree->right == NULL)
+	if (tree == NULL || tree->left == NULL)
 	{
 		return (NULL);
 	}
-	t_right = tree->right;
-	tree->right = t_right->left;
-	if (t_right->left != NULL)
+	t_left = tree->left;
+	tree->left = t_left->left;
+	if (t_left->left != NULL)
 	{
-		t_right->left->parent = tree;
+		t_left->left->parent = tree;
 	}
-	t_right->left = tree;
-	t_right->parent = tree->parent;
-	tree->parent = t_right;
-	return (t_right);
+	t_left->left = tree;
+	t_left->parent = tree->parent;
+	tree->parent = t_left;
+	return (t_left);
 }
